0132e3ef16576d0498467db47c8b6f502d7602b5
Zevi Liao <jointjm@gmail.com>
2019-07-29 15:09:49 +0800

    update

---
+++ src/components/HelloWorld-bak.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hello">
+    <div @click="showHide" class="drop-box gradient">change</div>
+
+    <div v-if="isExpand">
+      all
+      <div>
+        <ul>
+          <li>aa</li>
+          <li>bb</li>
+          <li>cc</li>
+        </ul>
+      </div>
+    </div>
+    <div v-else>aa, bb, cc, aa, bb, cc, aa, bb, cc, </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      isExpand: false
+    };
+  },
+  methods: {
+    showHide() {
+      this.isExpand = !this.isExpand;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.drop-box {
+  // width: 30px;
+  // height: 30px;
+  display: inline-block;
+  padding: 5px;
+  background: pink;
+  // float: right;
+  position: absolute;
+  right: 0;
+}
+
+.gradient {
+  background: linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 22%,
+    rgba(255, 255, 255, 1) 69%
+  );
+  background: -moz-linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 22%,
+    rgba(255, 255, 255, 1) 69%
+  );
+  background: -webkit-linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 22%,
+    rgba(255, 255, 255, 1) 69%
+  );
+  background: -o-linear-gradient(
+    90deg,
+    rgba(255, 255, 255, 0) 22%,
+    rgba(255, 255, 255, 1) 69%
+  );
+}
+
+.hello {
+  width: 200px;
+  background: lightblue;
+  position: relative;
+  padding: 5px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <!-- swiper -->
+    <swiper :options="swiperOption">
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide1+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide2+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide3+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide4+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide5+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide6+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide7+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide8+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide9+a" alt />
+      </swiper-slide>
+      <swiper-slide>
+        <img src="https://via.placeholder.com/150&text=Slide10+a" alt />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+    {{currentIndex}}
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import "swiper/dist/css/swiper.css";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
+export default {
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    const currentInstance = this;
+    return {
+      // swiperOption: {
+      //   slidesPerView: 4,
+      //   spaceBetween: 30,
+      //   centeredSlides: true
+      // }
+
+      swiperOption: {
+        loop: true,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        on: {
+          slideChange() {
+            currentInstance.currentIndex =
+              this.activeIndex % currentInstance.banners.length || 0;
+          }
+        },
+        autoplay: {
+          delay: 5000
+        },
+        speed: 1000
+      }
+    };
+  }
+};
+</script>
