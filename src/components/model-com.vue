7210f5603e84160fe50d555fd1285c382dc7bced
Zevi Liao <jointjm@gmail.com>
2019-07-31 11:56:43 +0800

    comp v-model

---
+++ src/components/model-com.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    hello
+    {{ value }}
+    <button @click="handleAddOne">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {};
+  },
+  methods: {
+    handleAddOne() {
+      let val = this.value + 1;
+      this.$emit("onAdd", val);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/model-lab.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <model-com v-model="pvalue" v-on:onAdd="handleUpdate" />
+    {{pvalue}}
+  </div>
+</template>
+
+<script>
+import ModelCom from "./model-com";
+export default {
+  data() {
+    return {
+      pvalue: 0
+    };
+  },
+  methods: {
+    handleUpdate(val) {
+      this.pvalue = val;
+    }
+  },
+  components: {
+    ModelCom
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
